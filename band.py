861cdf2cbce78bf3c5f1f3ddfc6bbca2af756ef6
NBLstudios <56075939+NBLstudios@users.noreply.github.com>
2020-12-19 20:25:02 +0530

    Add files via upload

---
+++ band.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("welcome to the NBL band name generator")
+#2. Ask the user for the city that they grew up in.
+city = input("what is name of city grow up in?\n")
+#3. Ask the user for the name of a pet.
+pet = input("what is your pet's name?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("your band name could be:  "+ city + " " + pet )
